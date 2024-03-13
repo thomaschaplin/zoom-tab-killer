016ccd149c5a045beb8c6d069c87990ee5e1daa4
Thomas Chaplin <thomaschaplin@outlook.com>
2024-03-13 14:41:04 +0000

    reduce timeout

--- zoomkiller.js
+++ zoomkiller.js
@@ -2,14 +2,14 @@ chrome.webNavigation.onCompleted.addListener((details) => {
     if (details.url.includes('zoom.us/postattendee')) {
         setTimeout(() => {
             chrome.tabs.remove(details.tabId, () => {});
-        }, 10000);
+        }, 3000);
     } else if (details.url.includes('zoom.us') && details.url.includes('status=success')) {
         setTimeout(() => {
             chrome.tabs.remove(details.tabId, () => {});
-        }, 10000);
+        }, 3000);
     } else if (details.url.includes('zoom.us') && details.url.endsWith('#success')) {
         setTimeout(() => {
             chrome.tabs.remove(details.tabId, () => {});
-        }, 10000);
+        }, 3000);
     }
 }, {url: [{schemes: ['https']}]});
